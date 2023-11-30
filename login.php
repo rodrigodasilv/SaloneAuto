5ca8931c68f333c1680a8fc93637fafbc6f6f6a7
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2023-11-30 15:48:59 -0300

    Recommit

--- login.php
+++ login.php
@@ -59,8 +59,8 @@
                 <?php if (isset($_GET['mensagem'])) { ?>
                     <div class="alert alert-warning alert-dismissible fade show" role="alert">
                         <?php echo $_GET['mensagem']; ?>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+                        <button style="bottom: 0 !important" type="button" class="close btn" data-dismiss="alert" aria-label="Close">
+                            &times;
                         </button>
                     </div>
                 <?php } ?>
