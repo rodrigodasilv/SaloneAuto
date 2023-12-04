9f49e4eaf4a10ab5b5b43dd76b3c2acedc88c01e
gabsiccardi <92187751+gabsiccardi@users.noreply.github.com>
2023-12-04 16:59:05 -0300

    Validações de formulário

--- cadastro.php
+++ cadastro.php
@@ -6,8 +6,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SaloneAuto - O lugar para encontrar seu novo automóvel!</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
@@ -15,9 +14,7 @@
 </head>
 
 <body style="width: 100%; padding: 0">
-    <header
-        class="text-bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom"
-        style="padding: 0 40px; border-bottom: 0 !important;">
+    <header class="text-bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom" style="padding: 0 40px; border-bottom: 0 !important;">
         <div class="col-md-3 mb-md-0 text-center" style="padding-right: 4rem">
             <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
                 <img src="./src/img/SaloneAuto2.png" class="img-fluid logo" alt="">
@@ -41,7 +38,7 @@
                 if (isset($_SESSION['user'])) {
                     header("Location: ./index.php");
                 }
-            }else{
+            } else {
                 echo '<a href="login.php"><button type="button" class="btn btn-outline-primary me-2">Login</button></a>';
             }
             ?>
@@ -53,7 +50,7 @@
             <div class="row">
                 <div class="col-sm-12 col-md-12">
                     <h2>Cadastre-se!</h2>
-                </div>  
+                </div>
             </div>
             <div class="col-12">
                 <?php if (isset($_GET['mensagem'])) { ?>
@@ -70,27 +67,27 @@
                     <form name="insuser" action="cadastroAct.php" method="post">
                         <div class="form-group">
                             <label for="idnome">Nome completo:</label>
-                            <input type="text" class="form-control" id="idnome" name="nome">
+                            <input type="text" class="form-control" id="idnome" name="nome" required>
                         </div>
                         <div class="form-group">
                             <label for="idemail">E-mail:</label>
-                            <input type="email" class="form-control" id="idemail" name="email">
+                            <input type="email" class="form-control" id="idemail" name="email" required>
                         </div>
                         <div class="form-group">
                             <label for="idsenha">Senha:</label>
-                            <input type="password" class="form-control" id="idsenha" name="senha">
+                            <input type="password" class="form-control" id="idsenha" name="senha" required>
                         </div>
                         <div class="form-group">
                             <label for="idcel">Celular:</label>
-                            <input type="text" class="form-control" id="idcel" name="celular">
+                            <input type="text" class="form-control" id="idcel" name="celular" required>
                         </div>
                         <div class="form-group">
                             <label for="idcpf">CPF:</label>
-                            <input type="text" class="form-control" id="idcpf" name="cpf">
+                            <input type="text" class="form-control" id="idcpf" name="cpf" maxlength="14" autocomplete="off" required>
                         </div>
                         <div class="form-group">
                             <label for="idNasc">Data de nascimento:</label>
-                            <input type="date" class="form-control" id="idNasc" name="nasc">
+                            <input type="date" class="form-control" id="idNasc" name="nasc" required>
                         </div>
                         <button type="reset" class="btn btn-danger" style="margin-top: 1rem; margin-bottom: 1rem">Limpar</button>
                         <button type="submit" class="btn btn-success" style="margin-top: 1rem; margin-bottom: 1rem">Enviar</button>
@@ -100,5 +97,41 @@
             </div>
         </div>
     </div>
+    <script>
+        const CPFInput = document.querySelector('#idcpf')
+        const foneInput = document.querySelector('#idcel')
+
+        CPFInput.addEventListener('keypress', () => {
+            let inputLength = CPFInput.value.length
+
+            if (inputLength === 3 || inputLength === 7) {
+                CPFInput.value += '.'
+            } else if (inputLength === 11) {
+                CPFInput.value += '-'
+            }
+        })
+
+        foneInput.addEventListener('input', () => {
+            let limparLetra = foneInput.value.replace(/\D/g, '').substring(0, 11)
+
+            let arrayNumero = limparLetra.split('')
+
+            let numeroFormatado = ''
+
+            if (arrayNumero.length > 0) {
+                numeroFormatado += `(${arrayNumero.slice(0, 2).join('')})`
+            }
+            if (arrayNumero.length > 2) {
+                numeroFormatado += ` ${arrayNumero.slice(2, 7).join('')}`
+            }
+
+            if (arrayNumero.length > 7) {
+                numeroFormatado += `-${arrayNumero.slice(7, 11).join('')}`
+            }
+
+            foneInput.value = numeroFormatado
+        })
+    </script>
 </body>
+
 </html>
--- vender.php
+++ vender.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,9 +15,9 @@
 <body class="vender">
     <header class="text-bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 border-bottom" style="padding: 0 40px; border-bottom: 0 !important;">
         <div class="col-md-3 mb-md-0 text-center" style="padding-right: 4rem">
-        <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
-            <img src="./src/img/SaloneAuto2.png" class="img-fluid logo" alt="">
-        </a>
+            <a href="index.php" class="d-inline-flex link-body-emphasis text-decoration-none">
+                <img src="./src/img/SaloneAuto2.png" class="img-fluid logo" alt="">
+            </a>
         </div>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -33,30 +34,31 @@
     <div class="container" style="padding-top: 2rem">
         <h3>Cadastre seu carro.</h3>
         <div class="col-12">
-                <?php if (isset($_GET['mensagem'])) { ?>
-                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                        <?php echo $_GET['mensagem']; ?>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                <?php } ?>
-            </div>
+            <?php if (isset($_GET['mensagem'])) { ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <?php echo $_GET['mensagem']; ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <?php } ?>
+        </div>
         <form action="venderAct.php" method="post" enctype="multipart/form-data">
             <label for="marcaSelect">Selecione a marca do carro</label><br>
             <script>
-                function addMarca(){
+                function addMarca() {
                     var lista = document.getElementById('modeloSelect');
                     var opcoes = lista.options;
 
                     for (var i = opcoes.length - 1; i >= 0; i--) {
                         opcoes[i].removeAttribute('hidden');
                         if (opcoes[i].getAttribute('marca') != document.getElementById('marcaSelect').value) {
-                        opcoes[i].setAttribute('hidden', true);
+                            opcoes[i].setAttribute('hidden', true);
                         }
                     }
                 }
-                function addModelo(){
+
+                function addModelo() {
                     var lista = document.getElementById('versaoSelect');
                     var opcoes = lista.options;
 
@@ -71,7 +73,7 @@
                     }
                 }
             </script>
-            <select class="form-select" id="marcaSelect" onchange="addMarca()">
+            <select class="form-select" id="marcaSelect" onchange="addMarca()" required>
                 <option value="">Selecione a marca!</option>
                 <?php
                 include_once('./validation.php');
@@ -86,7 +88,7 @@
                 ?>
             </select>
             <label for="modeloSelect">Selecione o modelo do carro</label><br>
-            <select class="form-select" id="modeloSelect" name="modelo" onchange="addModelo()">
+            <select class="form-select" id="modeloSelect" name="modelo" onchange="addModelo()" required>
                 <option value="">Selecione o modelo!</option>
                 <?php
                 $query = "SELECT * FROM modelos m";
@@ -94,26 +96,26 @@
                 $stm_sql->execute();
                 $modelos = $stm_sql->fetchAll(PDO::FETCH_ASSOC);
                 foreach ($modelos as $modelo) {
-                    echo '<option marca="'.$modelo['marcas_id'].'" value="' . $modelo['id_modelos'] . '">' . $modelo['nome_modelos'] . '</option>';
+                    echo '<option marca="' . $modelo['marcas_id'] . '" value="' . $modelo['id_modelos'] . '">' . $modelo['nome_modelos'] . '</option>';
                 }
                 ?>
             </select>
             <label for="versaoSelect">Selecione a versão do carro</label><br>
-            <select class="form-select" id="versaoSelect" name="versao">
+            <select class="form-select" id="versaoSelect" name="versao" required>
                 <option value="">Selecione a versão!</option>
-            <?php
+                <?php
                 $query = "SELECT * FROM versoes v";
                 $stm_sql = $banco->prepare($query);
                 $stm_sql->execute();
                 $versoes = $stm_sql->fetchAll(PDO::FETCH_ASSOC);
                 foreach ($versoes as $versao) {
-                    echo '<option modelo="'.$versao['modelos_id'].'" value="' . $versao['id_versoes'] . '">' . $versao['desc_versoes'] . '</option>';
+                    echo '<option modelo="' . $versao['modelos_id'] . '" value="' . $versao['id_versoes'] . '">' . $versao['desc_versoes'] . '</option>';
                 }
                 ?>
             </select>
             <label for="localizacao">Selecione a cor do carro</label>
             <br />
-            <select class="form-select" id="corSelect" name="cor">
+            <select class="form-select" id="corSelect" name="cor" required>
                 <option value="">Escolha uma cor</option>
                 <?php
                 $query = "SELECT * FROM cores";
@@ -127,7 +129,7 @@
             </select>
             <label for="localizacao">Selecione sua localização</label>
             <br />
-            <select class="form-select" name="localizacao" id="localizacao">
+            <select class="form-select" name="localizacao" id="localizacao" required>
                 <option value="">Escolha uma cidade</option>
                 <?php
                 $query = "SELECT * FROM cidades";
@@ -143,19 +145,19 @@
                 <div class="col-4">
                     <div class="form-group">
                         <label for="">Preço</label>
-                        <input class="form-control" type="decimal" name="price" id="price" placeholder="100000">
+                        <input class="form-control" type="decimal" name="price" id="price" placeholder="100000" required>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="form-group">
                         <label for="ultRev">Ano da última revisão:</label>
-                        <input class="form-control" id="ultRev" name="ultRev" type="number" placeholder="<?php echo date('Y') ?>">
+                        <input class="form-control" id="ultRev" name="ultRev" type="number" placeholder="<?php echo date('Y') ?>" required>
                     </div>
                 </div>
                 <div class="col-4">
                     <div class="form-group">
                         <label for="km">Quilometragem:</label>
-                        <input class="form-control" id="km" name="km" type="number" placeholder="50000">
+                        <input class="form-control" id="km" name="km" type="number" placeholder="50000" required>
                     </div>
                 </div>
             </div>
@@ -163,11 +165,11 @@
                 <div class="col-12">
                     <div class="form-group">
                         <label for="foto">Escolha uma foto do seu carro</label><br>
-                        <input type="file" class="form-control-file" id="foto" name="foto">
+                        <input type="file" class="form-control-file" id="foto" name="foto" required>
                     </div>
                 </div>
             </div>
-            
+
 
 
             <div class="text-center" style="padding-top: 2rem; padding-bottom: 2rem">
